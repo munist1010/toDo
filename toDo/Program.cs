905d59556bd84c55efeaa6c83af8fe73d5e910d3
munist1010 <aaronjkellywork@gmail.com>
2024-04-30 01:23:07 +0100

    Created a small command line to-do application in C#

---
+++ toDo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+
+List<string> toDo = new List<string>();
+
+while (true)
+{
+    if (toDo.Count > 0)
+    {
+        Console.WriteLine("Items on your to-do list: ");
+        for (int i = 0; i < toDo.Count; i++)
+        {
+            Console.WriteLine((i + 1).ToString() + " - " + toDo[i]);
+        }
+        Console.WriteLine("");
+    }
+    Console.WriteLine("1. Add a new task");
+    Console.WriteLine("2. Remove an existing task");
+    Console.WriteLine("3. Exit the program");
+    int choice = int.Parse(Console.ReadLine());
+
+    if (choice == (int)userInput.NewTask)
+    {
+        Console.Write("Enter new task: ");
+        string task = Console.ReadLine();
+        toDo.Add(task);
+        Console.Clear();
+        Console.WriteLine("New task added!");
+    }
+    else if (choice == (int)(userInput.DeleteTask))
+    {
+        if (toDo.Count > 0)
+        {
+            Console.WriteLine("Enter the number of the task you wish to remove");
+            for ( int i = 0;i < toDo.Count;i++)
+            {
+                Console.WriteLine("Task # " + (i+1).ToString() );
+            }
+        }
+        int taskToRemove = int.Parse(Console.ReadLine());
+        taskToRemove--;
+
+        if (taskToRemove >= 0 && taskToRemove < toDo.Count)
+        {
+            toDo.RemoveAt(taskToRemove);
+            Console.Clear();
+            Console.WriteLine("Task deleted!");
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Unable to remove task: invalid number");
+            Console.WriteLine("");
+        }
+    }
+    else if (choice == (int)(userInput.Exit))
+    {
+        break;
+    }
+}
+enum userInput
+{
+    NewTask = 1,
+    DeleteTask = 2,
+    Exit = 3,
+}
